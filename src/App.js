209d34fc7de02a31de7ab6252e47baecd76bd519
Minkyung Kim <min@Minkyungui-MacBookPro.local>
2020-09-11 18:25:26 +0900

    Add GPS API

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { SearchBar } from './components/SearchBar/SearchBar';
 import { Result } from './components/Result/Result';
 
@@ -7,9 +7,23 @@ const api = {
 }
 const apiKey = process.env.REACT_APP_API_KEY
 
+
 function App() {
   const [searchTerm, setSearchTerm] = useState("");
   const [weather, setWeather] = useState({});
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      const lat = position.coords.latitude
+      const lon = position.coords.longitude
+      fetch(`${api.base}weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`)
+        .then(res => res.json())
+        .then(data => {
+          console.log(data.name)
+          setWeather(data)
+        })
+    });
+  }, [])
 
   const search = (e) => {
     if (e.key === "Enter") {
@@ -22,13 +36,12 @@ function App() {
     }
   }
   const changeHandler = e => setSearchTerm(e.target.value)
-  
-  return (
-      <div className={weather.main !== undefined ? (weather.main.temp > 16 ? "app warm" : "app") : "app"}>
-        <SearchBar searchTerm={searchTerm} search={search} changeHandler={changeHandler}/>
-        <Result weather={weather}/>
-      </div>
 
+  return (
+    <div className={weather.main !== undefined ? (weather.main.temp > 16 ? "app warm" : "app") : "app"}>
+      <SearchBar searchTerm={searchTerm} search={search} changeHandler={changeHandler} />
+      <Result weather={weather} />
+    </div>
   )
 }
 export default App
