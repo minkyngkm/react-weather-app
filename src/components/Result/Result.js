2ddca38c7e389e5798a8b0f6cd8f3c9a0e34a70c
Minkyung Kim <min@Minkyungui-MacBookPro.local>
2020-09-11 20:03:49 +0900

    issue

--- src/components/Result/Result.js
+++ src/components/Result/Result.js
@@ -3,18 +3,27 @@ import styles from './Result.module.css'
 
 export function Result({ weather }) {
     const date = new Date()
-    return (
-    typeof weather.main != "undefined"  ?
-        (
-            <div className={styles.result}>
-                <div className={styles.date}> {date.toLocaleDateString()}</div>
-                <div className={styles.city}> {weather.name}, {weather.sys.country} </div>
-                <div className={styles.degree}> {Math.round(weather.main.temp)}°</div>
-                <div className={styles.description}> {weather.weather[0].description} </div>
-                <div className={styles.humidity}>  humidity : {weather.main.humidity} %</div>
-            </div>
-        )
-        : ("")
-    
+    weather.sys.country !== undefined ?
+    (
+        <div className={styles.result}>
+            <div className={styles.date}> {date.toLocaleDateString()}</div>
+            <div className={styles.city}> {weather.name}, {weather.sys.country} </div>
+            <div className={styles.degree}> {Math.round(weather.main.temp)}°</div>
+            <div className={styles.description}> {weather.weather[0].description} </div>
+            <div className={styles.humidity}>  humidity : {weather.main.humidity} %</div>
+        </div>
     )
+    : ""
 }
+
+// typeof weather.main != "undefined"  ?
+// (
+//     <div className={styles.result}>
+//         <div className={styles.date}> {date.toLocaleDateString()}</div>
+//         <div className={styles.city}> {weather.name}, {weather.sys.country} </div>
+//         <div className={styles.degree}> {Math.round(weather.main.temp)}°</div>
+//         <div className={styles.description}> {weather.weather[0].description} </div>
+//         <div className={styles.humidity}>  humidity : {weather.main.humidity} %</div>
+//     </div>
+// )
+// : ("")
