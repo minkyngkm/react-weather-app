7f5b4de36ec782c806beb634c5bef7ae893c6c78
minkyngkm <minkyngkm@gmail.com>
2020-09-16 18:10:12 +0900

    Update conditional rendering

--- src/components/Result/Result.js
+++ src/components/Result/Result.js
@@ -2,28 +2,18 @@ import React from 'react'
 import styles from './Result.module.css'
 
 export function Result({ weather }) {
-    const date = new Date()
-    weather.sys.country !== undefined ?
-    (
-        <div className={styles.result}>
-            <div className={styles.date}> {date.toLocaleDateString()}</div>
-            <div className={styles.city}> {weather.name}, {weather.sys.country} </div>
-            <div className={styles.degree}> {Math.round(weather.main.temp)}°</div>
-            <div className={styles.description}> {weather.weather[0].description} </div>
-            <div className={styles.humidity}>  humidity : {weather.main.humidity} %</div>
-        </div>
-    )
-    : ""
+    const date = new Date().toLocaleDateString()
+    
+    return(
+        weather.sys && weather.name && weather.main && weather.weather ?
+(
+    <div className={styles.result}>
+        <div className={styles.date}> {date}</div>
+        <div className={styles.city}> {weather.name}, {weather.sys.country} </div>
+        <div className={styles.degree}> {Math.round(weather.main.temp)}°</div>
+        <div className={styles.description}> {weather.weather[0].description} </div>
+        <div className={styles.humidity}>  humidity : {weather.main.humidity} %</div>
+    </div>
+) : "")
+       
 }
-
-// typeof weather.main != "undefined"  ?
-// (
-//     <div className={styles.result}>
-//         <div className={styles.date}> {date.toLocaleDateString()}</div>
-//         <div className={styles.city}> {weather.name}, {weather.sys.country} </div>
-//         <div className={styles.degree}> {Math.round(weather.main.temp)}°</div>
-//         <div className={styles.description}> {weather.weather[0].description} </div>
-//         <div className={styles.humidity}>  humidity : {weather.main.humidity} %</div>
-//     </div>
-// )
-// : ("")
